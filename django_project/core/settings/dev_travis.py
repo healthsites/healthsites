28e775eeefd70f706c2ec40bf2edb7c12965d3a8
Tim Sutton <tim@linfiniti.com>
2014-10-29 00:38:25 +0200

    First pass at a travis file

---
+++ django_project/core/settings/dev_travis.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+from .dev import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'gis',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        # Set to empty string for default.
+        'PORT': '',
+    }
+}
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        # define output formats
+        'verbose': {
+            'format': (
+                '%(levelname)s %(name)s %(asctime)s %(module)s %(process)d '
+                '%(thread)d %(message)s')
+        },
+        'simple': {
+            'format': (
+                '%(name)s %(levelname)s %(filename)s L%(lineno)s: '
+                '%(message)s')
+        },
+    },
+    'handlers': {
+        # console output
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple',
+            'level': 'DEBUG',
+        },
+        # 'logfile': {
+        #     'class': 'logging.FileHandler',
+        #     'filename': '/tmp/app-dev.log',
+        #     'formatter': 'simple',
+        #     'level': 'DEBUG',
+        # }
+    },
+    'loggers': {
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'INFO',  # switch to DEBUG to show actual SQL
+        },
+        # example app logger
+        'localities': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+            # propagate is True by default, which proppagates logs upstream
+            'propagate': False
+        }
+    },
+    # root logger
+    # non handled logs will propagate to the root logger
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING'
+    }
+}
