734c5168ed722357f6fe12d225e3d0bc01ea4871
Dražen Odobašić <dodobas@candela-it.com>
2014-10-28 18:14:10 +0100

    Add Locality form and tests

--- django_project/localities/forms.py
+++ django_project/localities/forms.py
@@ -4,9 +4,26 @@ LOG = logging.getLogger(__name__)
 
 import django.forms as forms
 
-# class aModelForm(forms.ModelForm):
-#     class Meta:
-#         model = aModel
 
-#     def __init__(self, *args, **kwargs):
-#         super(aModelForm, self).__init__(*args, **kwargs)
+class LocalityForm(forms.Form):
+    lon = forms.FloatField()
+    lat = forms.FloatField()
+
+    def __init__(self, locality, *args, **kwargs):
+
+        tmp_initial_data = {
+            'lon': locality.geom.x, 'lat': locality.geom.y
+        }
+
+        for attr in locality.group.attribute_set.all():
+            field = forms.CharField(label=attr.key)
+            self.base_fields[attr.key] = field
+
+        # Locality forms are special as they automatcally collect initial data
+        # based on the actual models
+        for value in locality.value_set.select_related('attribute').all():
+            tmp_initial_data.update({value.attribute.key: value.data})
+
+        kwargs.update({'initial': tmp_initial_data})
+
+        super(LocalityForm, self).__init__(*args, **kwargs)
---
+++ django_project/localities/tests/test_forms.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+from django.test import TestCase
+
+from .model_factories import LocalityValueF, GroupF, AttributeF, LocalityF
+
+from ..forms import LocalityForm
+
+
+class TestImporters(TestCase):
+    def test_dynamicformfields(self):
+        grp = GroupF.create(name='test')
+        AttributeF.create(key='test', in_groups=[grp])
+        AttributeF.create(key='osm', in_groups=[grp])
+
+        loc = LocalityF.create(geom='POINT(16 45)', group=grp)
+
+        frm = LocalityForm(loc)
+
+        self.assertEqual(
+            frm.as_ul(), (
+                u'<li><label for="id_lon">Lon:</label> <input id="id_lon" name'
+                u'="lon" step="any" type="number" value="45.0" /></li>\n<li><l'
+                u'abel for="id_lat">Lat:</label> <input id="id_lat" name="lat"'
+                u' step="any" type="number" value="16.0" /></li>\n<li><label f'
+                u'or="id_test">test:</label> <input id="id_test" name="test" t'
+                u'ype="text" /></li>\n<li><label for="id_osm">osm:</label> <in'
+                u'put id="id_osm" name="osm" type="text" /></li>'
+            )
+        )
+
+    def test_dynamicform_initialdata(self):
+        grp = GroupF()
+        test_attr = AttributeF.create(key='test', in_groups=[grp])
+
+        loc = LocalityValueF.create(
+            id=1, geom='POINT(16 45)', uuid='93b7e8c4621a4597938dfd3d27659162',
+            attr1__attribute=test_attr, attr1__data='osm', group=grp
+        )
+
+        frm = LocalityForm(loc)
+
+        self.assertEqual(
+            frm.as_ul(), (
+                u'<li><label for="id_lon">Lon:</label> <input id="id_lon" name'
+                u'="lon" step="any" type="number" value="45.0" /></li>\n<li><l'
+                u'abel for="id_lat">Lat:</label> <input id="id_lat" name="lat"'
+                u' step="any" type="number" value="16.0" /></li>\n<li><label f'
+                u'or="id_test">test:</label> <input id="id_test" name="test" t'
+                u'ype="text" value="osm" /></li>'
+            )
+        )
