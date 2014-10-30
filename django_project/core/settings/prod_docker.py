998e5bbc36603e28b281de5501dc5b4b43add482
Tim Sutton <tim@linfiniti.com>
2014-10-30 23:41:33 +0200

    Added prod file for docker

---
+++ django_project/core/settings/prod_docker.py
@@ -0,0 +1,18 @@
+from .project import *
+import os
+
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'gis',
+        'USER': os.environ['DATABASE_USERNAME'],
+        'PASSWORD': os.environ['DATABASE_PASSWORD'],
+        'HOST': os.environ['DATABASE_HOST'],
+        'PORT': 5432,
+        'TEST_NAME': 'unittests',
+    }
+}
+
+PIPELINE_YUGLIFY_BINARY = ABS_PATH('node_modules', 'yuglify', 'bin', 'yuglify')
