8985789775d2925be5227af2bbbdf1def098f5b8
Tim Sutton <tim@linfiniti.com>
2014-10-31 10:14:50 +0200

    Removed stray apostrophy

--- django_project/core/settings/prod_docker.py
+++ django_project/core/settings/prod_docker.py
@@ -6,7 +6,7 @@ ALLOWED_HOSTS = ['*']
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': os.environ['DATABASE_NAME']',
+        'NAME': os.environ['DATABASE_NAME'],
         'USER': os.environ['DATABASE_USERNAME'],
         'PASSWORD': os.environ['DATABASE_PASSWORD'],
         'HOST': os.environ['DATABASE_HOST'],
