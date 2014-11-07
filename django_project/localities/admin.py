390d94bc14933c7d4f2dacbac17c44955a7c2975
Dražen Odobašić <dodobas@candela-it.com>
2014-11-07 09:32:43 +0100

    Add changeset handling to the admin interface

--- django_project/localities/admin.py
+++ django_project/localities/admin.py
@@ -1,34 +1,39 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
 
-from .models import Domain, Locality, Value, Attribute, Specification
+from .models import (
+    Domain,
+    Attribute,
+    Specification,
+    Changeset
+)
 
 
-class DomainMA(admin.ModelAdmin):
-    pass
+class ChangesetMixin():
+    """
+    Record a changeset and increase version number
+    """
+    def save_model(self, request, obj, form, change):
+        if form.has_changed():
+            chgset = Changeset.objects.create()
+            obj.changeset = chgset
+            obj.inc_version()
+        obj.save()
+
+
+class DomainMA(ChangesetMixin, admin.ModelAdmin):
+    fields = ('name', 'description', 'template_fragment')
 
 admin.site.register(Domain, DomainMA)
 
 
-class LocalityMA(admin.ModelAdmin):
-    pass
-
-admin.site.register(Locality, LocalityMA)
-
-
-class ValueMA(admin.ModelAdmin):
-    pass
-
-admin.site.register(Value, ValueMA)
-
-
-class AttributeMA(admin.ModelAdmin):
-    pass
+class AttributeMA(ChangesetMixin, admin.ModelAdmin):
+    fields = ('key', 'description')
 
 admin.site.register(Attribute, AttributeMA)
 
 
-class SpecificationMA(admin.ModelAdmin):
-    pass
+class SpecificationMA(ChangesetMixin, admin.ModelAdmin):
+    fields = ('domain', 'attribute', 'required')
 
 admin.site.register(Specification, SpecificationMA)
