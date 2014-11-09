ea5e671633ea6960ed31bb420b32fba9215cf372
Igor Crnković <icrni@candela-it.com>
2014-11-09 12:13:03 +0100

    dev settings file fixes

--- django_project/core/settings/dev_igor.py
+++ django_project/core/settings/dev_igor.py
@@ -1,20 +1,6 @@
+# -*- coding: utf-8 -*-
 import os
-from .project import *
-
-# Set debug to True for development
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
-LOGGING_OUTPUT_ENABLED = DEBUG
-LOGGING_LOG_SQL = DEBUG
-
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
-# Disable caching while in development
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
-    }
-}
+from .dev import *  # noqa
 
 DATABASES = {
     'default': {
@@ -69,12 +55,12 @@ LOGGING = {
             'level': 'INFO',  # switch to DEBUG to show actual SQL
         },
         # example app logger
-        # 'app.module': {
-        #     'level': 'INFO',
-        #     'handlers': ['logfile'],
-        #     # propagate is True by default, which proppagates logs upstream
-        #     'propagate': False
-        # }
+        'localities': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+            # propagate is True by default, which proppagates logs upstream
+            'propagate': False
+        }
     },
     # root logger
     # non handled logs will propagate to the root logger
@@ -83,34 +69,3 @@ LOGGING = {
         'level': 'WARNING'
     }
 }
-
-# set up devserver if installed
-try:
-    import devserver
-    INSTALLED_APPS += (
-        'devserver',
-    )
-    # more details at https://github.com/dcramer/django-devserver#configuration
-    DEVSERVER_DEFAULT_ADDR = '0.0.0.0'
-    DEVSERVER_DEFAULT_PORT = '8000'
-    DEVSERVER_AUTO_PROFILE = False  # use decorated functions
-    DEVSERVER_TRUNCATE_SQL = True  # squash verbose output, show from/where
-    DEVSERVER_MODULES = (
-        # uncomment if you want to show every SQL executed
-        # 'devserver.modules.sql.SQLRealTimeModule',
-        # show sql query summary
-        'devserver.modules.sql.SQLSummaryModule',
-        # Total time to render a request
-        'devserver.modules.profile.ProfileSummaryModule',
-
-        # Modules not enabled by default
-        # 'devserver.modules.ajax.AjaxDumpModule',
-        # 'devserver.modules.profile.MemoryUseModule',
-        # 'devserver.modules.cache.CacheSummaryModule',
-        # see documentation for line profile decorator examples
-        # 'devserver.modules.profile.LineProfilerModule',
-        # show django session information
-        # 'devserver.modules.request.SessionInfoModule',
-    )
-except ImportError:
-    pass
