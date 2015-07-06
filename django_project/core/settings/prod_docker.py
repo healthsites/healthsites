2a1ed87db78c57c856aa2412bb6d7084cee2fe0e
Ismail Sunni <imajimatika@gmail.com>
2015-07-06 22:54:51 +0700

    Use kartoza/django-base.

--- django_project/core/settings/prod_docker.py
+++ django_project/core/settings/prod_docker.py
@@ -16,3 +16,20 @@ DATABASES = {
         'TEST_NAME': 'unittests',
     }
 }
+
+MEDIA_ROOT = '/home/web/media'
+STATIC_ROOT = '/home/web/static'
+
+# See docker-compose.yml file for postfix container definition
+#
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# Host for sending e-mail.
+EMAIL_HOST = 'smtp'
+# Port for sending e-mail.
+EMAIL_PORT = 25
+# SMTP authentication information for EMAIL_HOST.
+# See docker-compose.yml for where these are defined
+EMAIL_HOST_USER = 'noreply@healthsites.io'
+EMAIL_HOST_PASSWORD = 'docker'
+EMAIL_USE_TLS = False
+EMAIL_SUBJECT_PREFIX = '[healthsites]'
