4d9b2f772a249f9199d0c7dd718049997751a04a
Ismail Sunni <imajimatika@gmail.com>
2015-10-16 10:11:00 +0700

    Add LinkedIn sign in.

--- django_project/core/settings/contrib.py
+++ django_project/core/settings/contrib.py
@@ -2,13 +2,15 @@
 from .base import *  # noqa
 import os
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 # Extra installed apps
 INSTALLED_APPS += (
     'pipeline',
     'social.apps.django_app.default',
     'pg_fts',
     'django_forms_bootstrap',
-    'celery'
+    'celery',
 )
 
 # define template function (example for underscore)
@@ -30,8 +32,38 @@ AUTHENTICATION_BACKENDS = (
     'social.backends.github.GithubOAuth2',
     'social.backends.google.GoogleOAuth2',
     'social.backends.facebook.FacebookOAuth2',
+    'social.backends.linkedin.LinkedinOAuth2',
     'django.contrib.auth.backends.ModelBackend',
 )
+
+
+SOCIAL_AUTH_LINKEDIN_OAUTH2_SCOPE = [
+'r_basicprofile', 'r_emailaddress', 'rw_company_admin', 'w_share']
+
+SOCIAL_AUTH_LOGIN_REDIRECT_URL = '/'
+SOCIAL_AUTH_LOGIN_URL = '/'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.core.context_processors.debug',
+                'django.core.context_processors.i18n',
+                'django.core.context_processors.media',
+                'django.core.context_processors.static',
+                'django.core.context_processors.tz',
+                'django.contrib.messages.context_processors.messages',
+                'social.apps.django_app.context_processors.backends',
+                'social.apps.django_app.context_processors.login_redirect',
+            ],
+        },
+    },
+]
 
 BROKER_URL = 'amqp://guest:guest@%s:5672//' % os.environ['RABBITMQ_HOST']
 
