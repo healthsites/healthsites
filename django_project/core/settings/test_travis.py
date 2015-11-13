d993bb71d1976c16a3779419d7d08bbede859ec7
Tim Sutton <tim@kartoza.com>
2015-11-13 12:52:33 +0200

    Temporarily disable localities app to see if travis runs

--- django_project/core/settings/test_travis.py
+++ django_project/core/settings/test_travis.py
@@ -4,6 +4,11 @@ __author__ = 'rischan'
 # -*- coding: utf-8 -*-
 from .test import *  # noqa
 
+# Temporary hack as travis fails with
+# attribute error on ProjectState.render
+# whcih is caused by something in localities app
+INSTALLED_APPS = tuple(x for x in INSTALLED_APPS if x is not 'localities')
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
