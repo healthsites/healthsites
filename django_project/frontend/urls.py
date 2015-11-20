b84b5df6ca0c2be2b95afe5fb27be998d19d82d2
meomancer <meomancer@gmail.com>
2015-11-20 08:08:42 +0700

    256

--- django_project/frontend/urls.py
+++ django_project/frontend/urls.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import patterns, url
 
-from .views import MainView, AboutView, HelpView, MapView, map
+from .views import MainView, AboutView, HelpView, MapView, map, ContactView
 
 urlpatterns = patterns(
     '',
     # basic app views
     url(r'^$', MainView.as_view(), name='home'),
+    url(r'^contact', ContactView.as_view(), name='contact'),
     url(r'^about$', AboutView.as_view(), name='about'),
     url(r'^help', HelpView.as_view(), name='help'),
     # url(r'^map$', MapView.as_view(), name='map'),
