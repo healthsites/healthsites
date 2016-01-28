a297650ac5a2149dee0edf5376f9c993bb05673d
Maria Solovyeva <maria@kartoza.com>
2016-01-28 11:23:14 +0300

    UI fixes
    - menu splashing when search button clicked
    - initial home page background height
    - country autocomplete overlapping by map credits issue

--- django_project/core/base_static/js/custom-jquery.js
+++ django_project/core/base_static/js/custom-jquery.js
@@ -205,7 +205,7 @@ $(document).ready(function(){
 	   return false;
     });
 	
-	$('.nav a').on('click', function(){
+	$('.nav a').not('.navbar-search').on('click', function(){
     $('.btn-navbar').click(); //bootstrap 2.x
     $('.navbar-toggle').click() //bootstrap 3.x by Richard
 });
