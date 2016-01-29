935d924ee07aa5a72a78776042b6ec88f5681746
Tim Sutton <tim@kartoza.com>
2016-01-29 15:48:48 +0200

    Dont use tabs

--- django_project/core/base_static/js/custom-jquery.js
+++ django_project/core/base_static/js/custom-jquery.js
@@ -1,28 +1,28 @@
 $(document).ready(function(){
-	
-	//activate searchbar
-	$('.navbar-search').click(function() {
+
+  //activate searchbar
+  $('.navbar-search').click(function() {
       $("body").toggleClass("searchbar-active");
-	  $(".navbar-search i").toggleClass("fa-search fa-times");
-	  return false;
+    $(".navbar-search i").toggleClass("fa-search fa-times");
+    return false;
     });
-	
-	//activate social login/share
-	$('.navbar-share').click(function() {
+
+  //activate social login/share
+  $('.navbar-share').click(function() {
       $("#site-social").toggleClass("hidden");
-	  $(".navbar-share").toggleClass("closed");
-	   return false;
+    $(".navbar-share").toggleClass("closed");
+     return false;
     });
-	
-	$('.nav a').not('.navbar-search').on('click', function(){
+
+  $('.nav a').not('.navbar-search').on('click', function(){
     $('.btn-navbar').click(); //bootstrap 2.x
     $('.navbar-toggle').click() //bootstrap 3.x by Richard
 });
-	
-	$('.js-fullheight').css('height', $(window).height());
-	
-	
-	$('a[href*=#]:not([href=#])').click(function() {
+
+  $('.js-fullheight').css('height', $(window).height());
+
+
+  $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
@@ -30,86 +30,86 @@ $(document).ready(function(){
         $('html,body').animate({
           scrollTop: target.offset().top
         }, 1000);
-		   window.history.pushState("", "", '#'+this.hash.slice(1));
+       window.history.pushState("", "", '#'+this.hash.slice(1));
         return false;
       }
     }
   });
-	
-   
+
+
   $("#locality-name").bind("DOMSubtreeModified",function(){
       $(this).parent().stop().animate({ backgroundColor: "#f44a52",
-	                                    color: "#fff" }, 100)
-	                         .animate({ backgroundColor: "white",
-							            color: "#3c4c57" }, 550);
+                                      color: "#fff" }, 100)
+                           .animate({ backgroundColor: "white",
+                          color: "#3c4c57" }, 550);
   });
-  
+
   var v_width = $(window).width();
   var v_height = $(window).height();
   var h_height = $('.masthead').height();
   var s_width = $('.location-info').width();
-  
+
   if (v_width<=991) {
-	  
-      
-     
-	  var pad = v_height/2;
-	  $('.location-info').css('margin-top',pad);
-	  $('.map-page #map, #map-home').css('height',pad);
-	  $('.map-page #map, #map-home').css('width',v_width);
-	   
+
+
+
+    var pad = v_height/2;
+    $('.location-info').css('margin-top',pad);
+    $('.map-page #map, #map-home').css('height',pad);
+    $('.map-page #map, #map-home').css('width',v_width);
+
       //mobile country search
-	$('.select-country input').focusin(function() {
-		$('body').addClass('search-focus');
-	});
-	$('.select-country input').focusout(function() {
-		$('body').removeClass('search-focus');
-	});
- 
+  $('.select-country input').focusin(function() {
+    $('body').addClass('search-focus');
+  });
+  $('.select-country input').focusout(function() {
+    $('body').removeClass('search-focus');
+  });
+
   }
-	else {
-		 $('.location-info').css('margin-top',0);
+  else {
+     $('.location-info').css('margin-top',0);
          $('.map-page #map').css('height', v_height - h_height);
          $('.map-page #map').css('width', v_width - s_width);
-		 
-		 
-	}
-	
-	var v_count = '0';
-	
-	
-	
-	$('.timer').each(function(){
-			var imagePos = $(this).offset().top;			
-			var topOfWindow = $(window).scrollTop();
-			if (imagePos < topOfWindow+500 && v_count=='0') {		
-				$(function ($) {
-					// start all the timers
-					$('.timer').each(count);					  					  
-					function count(options) {
-						v_count = '1';
-						var $this = $(this);
-						options = $.extend({}, options || {}, $this.data('countToOptions') || {});
-						$this.countTo(options);
-					}
-				});				
-			}
-		});
-		
+
+
+  }
+
+  var v_count = '0';
+
+
+
+  $('.timer').each(function(){
+      var imagePos = $(this).offset().top;
+      var topOfWindow = $(window).scrollTop();
+      if (imagePos < topOfWindow+500 && v_count=='0') {
+        $(function ($) {
+          // start all the timers
+          $('.timer').each(count);
+          function count(options) {
+            v_count = '1';
+            var $this = $(this);
+            options = $.extend({}, options || {}, $this.data('countToOptions') || {});
+            $this.countTo(options);
+          }
+        });
+      }
+    });
+
 
 });
-	
+
 //hide/show tagline on scroll
-$(window).scroll(function() {    
+$(window).scroll(function() {
   var scroll = $(window).scrollTop();
   if (scroll >= 72) {
-	  $(".home .masthead").addClass("no-tag");
+    $(".home .masthead").addClass("no-tag");
   } else {
-	  $(".home .masthead").removeClass("no-tag");
-  }	
-  
+    $(".home .masthead").removeClass("no-tag");
+  }
 
-  
+
+
 });
 
 
@@ -123,16 +123,16 @@ $(window).resize(function() {
   $('.map-page #map').css('height', v_height - h_height);
   $('.map-page #map').css('width', v_width - s_width);
   if (v_width<=991) {
-	  var pad = v_height/2;
-	  $('.location-info').css('margin-top',pad);
-	  $('.map-page #map, #map-home').css('height',pad);
-	  $('.map-page #map, #map-home').css('width',v_width);
-	  var getslidewd= $('.main-img').width();
+    var pad = v_height/2;
+    $('.location-info').css('margin-top',pad);
+    $('.map-page #map, #map-home').css('height',pad);
+    $('.map-page #map, #map-home').css('width',v_width);
+    var getslidewd= $('.main-img').width();
   }
   else {
-	   $('.location-info').css('margin-top',0);
-	   $('#map-home').css('width','50%');
-	   $('#map-home').css('height', 400);
+     $('.location-info').css('margin-top',0);
+     $('#map-home').css('width','50%');
+     $('#map-home').css('height', 400);
   }
 });
 
