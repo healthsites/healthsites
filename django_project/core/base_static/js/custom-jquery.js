f9445551725cfd4937875e7455baf188af932501
Maria Solovyeva <maria@kartoza.com>
2016-02-18 10:08:32 +0300

    MSF logo

--- django_project/core/base_static/js/custom-jquery.js
+++ django_project/core/base_static/js/custom-jquery.js
@@ -19,7 +19,7 @@ function mapcount() {
 
 	  var pad = r_height/2;
 	  $('.location-info').css('margin-top',h_height + pad);
-	  $('#map').css('top', h_height);
+	  $('.map-page #map').css('top', h_height);
 	  $('#map, #map-home').css('height',pad);
 	  $('#map, #map-home').css('width',v_width);
 
