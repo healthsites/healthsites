b01723fd1030a242be5ac88c0ec6254c87439724
meomancer <meomancer@gmail.com>
2016-02-24 12:45:50 +0700

    bug when screen name is empty

--- django_project/social_users/views.py
+++ django_project/social_users/views.py
@@ -26,6 +26,7 @@ def getProfile(user):
     shared_links = []
     # check if the user has profile_picture
     # if not, just send empty string
+    username = user.username
     try:
         user_detail = Profile.objects.get(user=user)
         profile_picture = user_detail.profile_picture
@@ -34,7 +35,7 @@ def getProfile(user):
         profile_picture = ""
 
     user.profile_picture = profile_picture
-    user.username = username
+    user.screen_name = username
     user.shared_links = shared_links
     return user
 
@@ -75,7 +76,7 @@ def save_profile(backend, user, response, *args, **kwargs):
             username = name
             try:
                 name = response['last_name']
-                username += name
+                username += " "+name
             except Exception as exept:
                 print exept
         except Exception as exept:
