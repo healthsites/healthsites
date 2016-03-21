6294ac3354ee3ed4151f81ac01a64bc1a7239ffb
Maria Solovyeva <maria@kartoza.com>
2016-03-21 17:39:25 +0300

    #642, #640, #655
    - Favicon
    - Profile page on mobile - avatar fix
    - Mobile optimisation for cookies

--- django_project/frontend/static/js/cookie-bar.js
+++ django_project/frontend/static/js/cookie-bar.js
@@ -33,7 +33,7 @@
             acceptAnyClick: false, //Set to true to accept cookies when visitor clicks anywhere on the page
             expireDays: 365, //Number of days for cookieBar cookie to be stored for
             renewOnVisit: false, //Renew the cookie upon revisit to website
-            forceShow: true, //Force cookieBar to show regardless of user cookie preference
+            forceShow: false, //Force cookieBar to show regardless of user cookie preference
             effect: 'slide', //Options: slide, fade, hide
             element: 'body', //Element to append/prepend cookieBar to. Remember "." for class or "#" for id.
             append: false, //Set to true for cookieBar HTML to be placed at base of website. Actual position may change according to CSS
