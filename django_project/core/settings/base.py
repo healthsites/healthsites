97c12998810ab116cae9104fc74123c66a4915b2
meomancer <meomancer@gmail.com>
2016-03-23 15:27:46 +0700

    fixing setting bug that break CSV imprter

--- django_project/core/settings/base.py
+++ django_project/core/settings/base.py
@@ -44,7 +44,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = absolute_path('media')
+MEDIA_ROOT = '/home/web/media'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -57,7 +57,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = absolute_path('static')
+STATIC_ROOT = '/home/web/static'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
