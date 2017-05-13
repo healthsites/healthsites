a77971ccd814cc6ef35318513d3b90dc855f6775
patrick nsukami <ndkpatt@gmail.com>
2017-05-13 16:12:08 +0000

    Remove dead code

--- django_project/localities/models.py
+++ django_project/localities/models.py
@@ -291,7 +291,6 @@ class Locality(UpdateMixin, ChangesetMixin):
             pass
 
         # exclusive for open street map
-        # if "openstreetmap" in self.upstream_id.lower():
         if self.upstream_id is not None and "openstreetmap" in self.upstream_id.lower():
             osm_whole_id = self.upstream_id.split(u"¶")
             if len(osm_whole_id) > 0:
