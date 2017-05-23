fd1c1121863c9a532a11c388cd4dac6cfc8584ef
Dražen Odobašić <dodobas@candela-it.com>
2017-05-23 14:41:35 +0200

    Remove django-devserver package

--- django_project/core/settings/dev.py
+++ django_project/core/settings/dev.py
@@ -55,35 +55,3 @@ LOGGING = {
         'level': 'WARNING'
     }
 }
-
-# set up devserver if installed
-try:
-    # noinspection PyUnresolvedReferences
-    import devserver  # noqa
-    INSTALLED_APPS += (
-        'devserver',
-    )
-    # more details at https://github.com/dcramer/django-devserver#configuration
-    DEVSERVER_DEFAULT_ADDR = '0.0.0.0'
-    DEVSERVER_DEFAULT_PORT = '8000'
-    DEVSERVER_AUTO_PROFILE = False  # use decorated functions
-    DEVSERVER_TRUNCATE_SQL = True  # squash verbose output, show from/where
-    DEVSERVER_MODULES = (
-        # uncomment if you want to show every SQL executed
-        # 'devserver.modules.sql.SQLRealTimeModule',
-        # show sql query summary
-        'devserver.modules.sql.SQLSummaryModule',
-        # Total time to render a request
-        'devserver.modules.profile.ProfileSummaryModule',
-
-        # Modules not enabled by default
-        # 'devserver.modules.ajax.AjaxDumpModule',
-        # 'devserver.modules.profile.MemoryUseModule',
-        # 'devserver.modules.cache.CacheSummaryModule',
-        # see documentation for line profile decorator examples
-        # 'devserver.modules.profile.LineProfilerModule',
-        # show django session information
-        # 'devserver.modules.request.SessionInfoModule',
-    )
-except ImportError:
-    pass
