d07cb56cb03e0282ea6ce9465a123248d1c58d2c
Dražen Odobašić <dodobas@candela-it.com>
2017-06-16 10:03:01 +0200

    Add tests for middleware functions

--- django_project/social_users/middleware.py
+++ django_project/social_users/middleware.py
@@ -9,17 +9,10 @@ LOG = logging.getLogger(__name__)
 
 
 def save_profile(backend, user, response, *args, **kwargs):
-    url = None
-    if backend.name == 'facebook':
-        url = 'http://graph.facebook.com/%s/picture?type=large' % response['id']
-    if backend.name == 'twitter':
-        url = response.get('profile_image_url', '').replace('_normal', '')
-    if backend.name == 'google-oauth2':
-        url = response['image'].get('url')
     # get old user
     if kwargs['is_new']:
-        if 'username' in kwargs['details']:
-            new_username = kwargs['details']['username']
+        new_username = kwargs['details'].get('username')
+        if new_username:
             new_username = new_username.replace(' ', '')
             if user.username != new_username:
                 try:
@@ -32,7 +25,22 @@ def save_profile(backend, user, response, *args, **kwargs):
         profile = Profile.objects.get(user=user)
     except Profile.DoesNotExist:
         profile = Profile(user=user)
+
+    url = profile_picture_url(backend=backend, response=response)
     if url:
         profile.profile_picture = url
     profile.save()
     return {'user': user}
+
+
+def profile_picture_url(backend, response):
+    url = None
+
+    if backend.name == 'facebook':
+        url = 'http://graph.facebook.com/%s/picture?type=large' % response['id']
+    if backend.name == 'twitter':
+        url = response.get('profile_image_url', '').replace('_normal', '')
+    if backend.name == 'google-oauth2':
+        url = response['image'].get('url')
+
+    return url
