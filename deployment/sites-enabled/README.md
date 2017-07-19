efec430639471e4cad8d98ca6b7a0918dbdda99a
Dražen Odobašić <dodobas@candela-it.com>
2017-07-19 13:22:11 +0200

    Remove old deployment code

--- deployment/sites-enabled/README.md
+++
@@ -1,4 +0,0 @@
-This directory is used to contain nginx configuration
-when using a non dockergen nginx configuration.
-
-See docker-compose.yml to see how it is used.
--- django_project/core/settings/dev_dodobas.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from os.path import abspath, dirname, join, pardir
-
-from .dev import *  # noqa
-
-PROJECT_PATH = abspath(join(dirname(__file__), pardir, pardir))
-MEDIA_ROOT = join(PROJECT_PATH, 'media')
-STATIC_ROOT = join(PROJECT_PATH, 'static')
-
