9a142acb4c63356ca5436a8e6a621d0303a0119a
ann26 <anita@kartoza.com>
2019-04-25 16:35:40 +0700

    add utility to convert into osm attr and add utility to split osm and extension attr

---
+++ django_project/localities_osm/tests/test_utilities.py
@@ -0,0 +1,91 @@
+# coding=utf-8
+__author__ = 'Anita Hapsari <anita@kartoza.com>'
+__date__ = '25/02/19'
+
+from django.test import TestCase
+from localities_osm.utilities import (
+    convert_into_osm_dict,
+    split_osm_and_extension_attr
+)
+from localities_osm.models.locality import LocalityOSM
+from localities.tests.model_factories import (
+    AttributeF,
+    LocalityValue4F
+)
+from social_users.tests.model_factories import UserF
+
+
+class AttributeConverterUtilityTest(TestCase):
+
+    def setUp(self):
+        self.user = UserF.create(**{
+            'username': 'anita',
+            'is_staff': True
+        })
+        self.user.set_password('password')
+        self.user.save()
+        self.attr = AttributeF.create(key='facility')
+        self.attr2 = AttributeF.create(key='staff')
+        self.attr3 = AttributeF.create(key='inpatient_service')
+        self.attr4 = AttributeF.create(key='scope_of_service')
+        self.locality = LocalityValue4F.create(
+            name='locality test',
+            val1__data='building',
+            val1__specification__attribute=self.attr,
+            val2__data='15|18',
+            val2__specification__attribute=self.attr2,
+            val3__data='5|7',
+            val3__specification__attribute=self.attr3,
+            val4__data=
+            'specialized care|general acute care|rehabilitation care|',
+            val4__specification__attribute=self.attr4,
+        )
+
+    def test_convert_into_osm_dict(self):
+        """Test converting old locality dict into osm dict."""
+
+        osm_data_dict = convert_into_osm_dict(self.locality)
+        self.assertEqual(osm_data_dict['name'], 'locality test')
+        self.assertEqual(osm_data_dict['staff_nurses'], '18')
+        self.assertEqual(osm_data_dict['staff_doctors'], '15')
+        self.assertEqual(osm_data_dict['inpatient_service'], '12')
+        self.assertEqual(
+            osm_data_dict['scope_of_service'],
+            ['specialized care', 'general acute care', 'rehabilitation care'])
+        self.assertEqual(osm_data_dict['facility'], 'building')
+
+    def test_split_osm_attr_and_extension(self):
+        """Test splitting osm attributes and extension attributes."""
+
+        osm_data_dict = convert_into_osm_dict(self.locality)
+        osm_attr, extension_attr = \
+            split_osm_and_extension_attr(osm_data_dict)
+
+        self.assertEqual(extension_attr['facility'], 'building')
+        self.assertEqual(
+            extension_attr['scope_of_service'],
+            ['specialized care', 'general acute care', 'rehabilitation care'])
+        self.assertEqual(
+            extension_attr.keys(), ['facility', 'scope_of_service'])
+        self.assertItemsEqual(
+            osm_attr.keys(),
+            ['speciality', 'osm_id', 'operator', 'operation', 'water_source',
+             'changeset_id', 'insurance', 'staff_doctors', 'category',
+             'contact_number', 'raw_data_archive_url', 'source', 'type',
+             'status', 'wheelchair_access', 'physical_address', 'emergency',
+             'changeset_timestamp', 'nature_of_facility', 'ownership', 'name',
+             'staff_nurses', 'changeset_user', 'changeset_version',
+             'dispensing', 'power_source', 'inpatient_service']
+        )
+
+        osm_fields = LocalityOSM._meta.get_all_field_names()
+        self.assertItemsEqual(
+            osm_attr.keys(),
+            osm_fields
+        )
+
+        self.assertEqual(osm_attr['inpatient_service'], '12')
+        self.assertEqual(osm_attr['physical_address'], '')
+        self.assertEqual(osm_attr['name'], 'locality test')
+        self.assertEqual(osm_attr['staff_nurses'], '18')
+        self.assertEqual(osm_attr['staff_doctors'], '15')
--- django_project/localities_osm/utilities.py
+++ django_project/localities_osm/utilities.py
@@ -1,8 +1,50 @@
 __author__ = 'Irwan Fathurrahman <irwan@kartoza.com>'
 __date__ = '04/04/19'
 
-from localities_osm.models.locality import LocalityOSMView
+from localities_osm.models.locality import LocalityOSMView, LocalityOSM
 
 
 def get_all_osm_query():
     return LocalityOSMView.objects.filter(osm_id__isnull=False).exclude(name='')
+
+
+def convert_into_osm_dict(locality):
+    data = locality.repr_dict()
+
+    osm_dict = data['values']
+    osm_dict['name'] = data['name']
+
+    if 'staff' in osm_dict.keys():
+        staff = osm_dict['staff'].split('|')
+        osm_dict['staff_doctors'] = staff[0]
+        osm_dict['staff_nurses'] = staff[1]
+        del osm_dict['staff']
+
+    if 'inpatient_service' in osm_dict.keys():
+        beds = osm_dict['inpatient_service'].split('|')
+        total_bed = int(beds[0]) + int(beds[1])
+        osm_dict['inpatient_service'] = str(total_bed)
+
+    for item, value in osm_dict.items():
+        if '|' in value:
+            value = value.split('|')
+            osm_dict[item] = list(filter(None, value))
+
+    return osm_dict
+
+
+def split_osm_and_extension_attr(locality_attr):
+    osm_fields = LocalityOSM._meta.get_all_field_names()
+
+    osm_attr = {}
+    for field in osm_fields:
+        if field in locality_attr.keys():
+            osm_attr[field] = locality_attr[field]
+            del locality_attr[field]
+        else:
+            osm_attr[field] = ''
+
+    if 'defining_hours' in locality_attr.keys():
+        del locality_attr['defining_hours']
+
+    return [osm_attr, locality_attr]
