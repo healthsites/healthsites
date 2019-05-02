d1f22ac1112e095d79bb8a1376b6b1772f00b852
meomancer <meomancer@gmail.com>
2019-05-02 13:21:37 +0700

    update mapping data with finalized data

--- django_project/localities_osm/admin.py
+++ django_project/localities_osm/admin.py
@@ -8,8 +8,8 @@ from localities_osm.models.locality import (
 
 
 class LocalityOSMAdmin(admin.ModelAdmin):
-    list_display = ('osm_id', 'category', 'name', 'changeset_timestamp', 'changeset_user')
-    list_filter = ('type',)
+    list_display = ('osm_id', 'amenity', 'name', 'changeset_timestamp', 'changeset_user')
+    list_filter = ('healthcare',)
     ordering = ('name',)
     search_fields = ('name', 'osm_id')
     readonly_fields = ('osm_id',)
@@ -17,8 +17,8 @@ class LocalityOSMAdmin(admin.ModelAdmin):
 
 class LocalityOSMViewAdmin(LocalityOSMAdmin):
     list_display = (
-        'osm_id', 'category', 'name', 'osm_type', 'changeset_timestamp', 'changeset_user', 'ownership')
-    list_filter = ('category', 'osm_type')
+        'osm_id', 'amenity', 'name', 'osm_type', 'changeset_timestamp', 'changeset_user', 'operator')
+    list_filter = ('amenity', 'osm_type')
     readonly_fields = ('osm_id', 'row', 'osm_type')
 
 
