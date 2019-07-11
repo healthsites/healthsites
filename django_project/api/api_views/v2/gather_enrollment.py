791b55e53bbfd7c823e04e8a2cb3f5ecd925b32a
meomancer <meomancer@gmail.com>
2019-07-11 17:35:27 +0700

    reset password if user exist

--- django_project/api/api_views/v2/gather_enrollment.py
+++ django_project/api/api_views/v2/gather_enrollment.py
@@ -27,41 +27,77 @@ except AttributeError:
 class GatherEnrollment(BaseAPIWithAuth):
     """API to get the Gather ODK server settings."""
 
+    def get_gather(self, server_url):
+        """ GET gather """
+        session = requests.Session()
+        session.auth = (GATHER_USERNAME, GATHER_PASSWORD)
+        r = session.get(server_url)
+        return r
+
+    def post_gather(self, server_url, payload):
+        """ For post gather """
+        # POST with form-encoded data
+        session = requests.Session()
+        session.auth = (GATHER_USERNAME, GATHER_PASSWORD)
+
+        r = session.head(server_url)
+        csrf_token = r.cookies.get('csrftoken')
+
+        headers = {'X-csrftoken': csrf_token}
+        r = session.post(server_url, json=payload, headers=headers)
+        return r
+
+    def put_gather(self, server_url, payload):
+        """ For post gather """
+        # POST with form-encoded data
+        session = requests.Session()
+        session.auth = (GATHER_USERNAME, GATHER_PASSWORD)
+
+        r = session.head(server_url)
+        csrf_token = r.cookies.get('csrftoken')
+
+        headers = {'X-csrftoken': csrf_token}
+        r = session.put(server_url, json=payload, headers=headers)
+        return r
+
     def get(self, request):
-        try:
-            gather_user = GatherUser.objects.get(user=request.user)
-            password = gather_user.gather_password
-        except GatherUser.DoesNotExist:
-            password_characters = string.ascii_letters + string.digits + string.punctuation
-            # generate payload
-            password = ''.join(random.choice(password_characters) for i in range(10))
-            server_url = settings.GATHER_API_URL + 'odk/surveyors/'
+        password_characters = string.ascii_letters + \
+                              string.digits + string.punctuation
+        # generate payload
+        password = ''.join(random.choice(password_characters) for i in range(10))
+        payload = {
+            'username': request.user.username,
+            'password': password}
 
-            payload = {
-                'username': request.user.username,
-                'password': password}
+        # GENERATE SURVEYOR
+        response = self.post_gather(settings.GATHER_API_URL + 'odk/surveyors/', payload)
+        if response.status_code == 201 or response.status_code == 200:
+            pass
+        elif response.status_code == 400:
+            # ALREADY EXIST
+            # CHANGE THE PASSWORD
+            # SEARCH USER ID
+            server_url = (settings.GATHER_API_URL +
+                          'odk/surveyors/?search=%s' % request.user.username)
+            response = self.get_gather(server_url)
+            content = json.loads(response.content)
+            if response.status_code == 201 or response.status_code == 200:
+                for result in content['results']:
+                    if result['username'] == request.user.username:
+                        # IF USERNAME FOUND RESET PASSWORD
+                        response = self.put_gather(
+                            settings.GATHER_API_URL + 'odk/surveyors/%s/' % result['id'], payload)
+                        if response.status_code == 201 or response.status_code == 200:
+                            pass
+                        else:
+                            return HttpResponseBadRequest(response.text)
+                        break
 
-            # POST with form-encoded data
-            session = requests.Session()
-            session.auth = (GATHER_USERNAME, GATHER_PASSWORD)
-
-            r = session.head(server_url)
-            csrf_token = r.cookies.get('csrftoken')
-
-            headers = {'X-csrftoken': csrf_token}
-            r = session.post(server_url, json=payload, headers=headers)
-            if r.status_code == 201 or r.status_code == 200:
-                content = json.loads(r.content)
-                try:
-                    gather_user = GatherUser(user=request.user)
-                except GatherUser.DoesNotExist:
-                    gather_user = GatherUser()
-                gather_user.user = request.user
-                gather_user.gather_id = content['id']
-                gather_user.gather_password = password
-                gather_user.save()
             else:
-                return HttpResponseBadRequest(r.text)
+                return HttpResponseBadRequest(response.text)
+
+        else:
+            return HttpResponseBadRequest(response.text)
 
         data = {
             'general': {
