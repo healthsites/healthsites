dfc52b556a0237ecfd0bffc372708dc09c5782d7
ann26 <anita@kartoza.com>
2019-07-16 16:02:50 +0700

    update js

--- django_project/frontend/static/scripts/configs/common.js
+++ django_project/frontend/static/scripts/configs/common.js
@@ -7,57 +7,31 @@ require.config({
         "underscore": "libs/underscore.js/1.9.1/underscore-min",
         "bootstrap": "libs/bootstrap/3.3.5/js/bootstrap.min",
         "d3": "libs/d3/3.5.7/d3.min",
-        "c3": "libs/c3/0.4.22/c3.min",
+        "c3": "libs/c3/0.6.14/c3.min",
         "leaflet": "libs/leaflet/0.7.7/leaflet-src",
-        "slickmin": "libs/jquery.slick/1.5.7/slick.min",
-        "leafletDraw": "libs/leaflet.draw/0.2.3/leaflet.draw-src",
-        "signals": "libs/js-signals/1.0.0/js-signals.min",
-        "app": "js/app",
-        "cluster-layer": "js/cluster-layer",
-        "map-functionality": "js/map-functionality",
-        "hasher": "libs/hasher/1.2.0/hasher.min",
-        "crossroads": "libs/crossroads/0.12.2/crossroads.min",
+        'map-functionality': 'scripts/views/map/map',
+        'leafletDraw': 'libs/leaflet.draw/0.2.3/leaflet.draw-src'
     },
     shim: {
-        jquery : {
-            exports: ['$', 'jQuery']
-        },
-        "jquery-ui" : {
-            exports: ['jqueryUI']
-        },
-        bootstrap : {
-            deps : [ 'jquery'],
-            exports: 'Bootstrap'
-        },
-        underscore : {
-            exports : '_'
-        },
-        backbone : {
-            deps : [ 'jquery', 'underscore' ],
-            exports : 'Backbone'
-        },
-        leaflet : {
+        leaflet: {
             exports: ['L']
         },
-        leafletDraw : {
-            deps : [ 'leaflet'],
+        'static/scripts/views/map/cluster.js': {
+            deps: ['leaflet'],
+        },
+        leafletDraw: {
+            deps: ['leaflet'],
             exports: 'LeafletDraw'
         },
-        slickmin : {
-            exports: [ 'slickMin' ]
-        },
-        signals : {
-            exports: [ 'signals' ]
+        signals: {
+            exports: ['signals']
         },
         crossroads: {
             deps: ['backbone', 'jquery', 'signals'],
             exports: ['crossroads']
         },
-        'cluster-layer': {
-            deps : [ 'leaflet'],
-        },
         'map-functionality': {
-            deps : [ 'leaflet', 'cluster-layer', 'leafletDraw' ]
+            deps: ['leaflet', 'leafletDraw']
         },
     }
 });
--- django_project/frontend/static/scripts/configs/index.js
+++ django_project/frontend/static/scripts/configs/index.js
@@ -1,74 +1,38 @@
-require.config({
-    baseUrl: '/static/',
-    paths: {
-        "jquery": "libs/jquery.js/3.3.1/jquery.min",
-        "jquery-ui": "libs/jquery.js/1.12.1/jquery-ui.min",
-        "backbone": "libs/backbone.js/1.4.0/backbone-min",
-        "underscore": "libs/underscore.js/1.9.1/underscore-min",
-        "bootstrap": "libs/bootstrap/3.3.5/js/bootstrap.min",
-        "d3": "libs/d3/3.5.7/d3.min",
-        "c3": "libs/c3/0.6.14/c3.min",
-        "leaflet": "libs/leaflet/0.7.7/leaflet-src",
-        'map-functionality': 'scripts/views/map/map',
-        'leafletDraw': 'libs/leaflet.draw/0.2.3/leaflet.draw-src'
-    },
-    shim: {
-        leaflet: {
-            exports: ['L']
-        },
-        'static/scripts/views/map/cluster.js': {
-            deps: ['leaflet'],
-        },
-        leafletDraw: {
-            deps: ['leaflet'],
-            exports: 'LeafletDraw'
-        },
-        signals: {
-            exports: ['signals']
-        },
-        crossroads: {
-            deps: ['backbone', 'jquery', 'signals'],
-            exports: ['crossroads']
-        },
-        'map-functionality': {
-            deps: ['leaflet', 'leafletDraw']
-        },
-    }
-});
-require([
-    'jquery',
-    'backbone',
-    'underscore',
-    'leaflet',
-    'static/scripts/views/map/cluster.js',
-    'map-functionality',
-    'static/scripts/parameters.js',
-    'static/scripts/shared.js',
-    'static/scripts/views/statistic/search.js',
-    'static/scripts/views/statistic/view.js',
-    'static/scripts/views/navbar/search.js',
-    'static/scripts/views/map/app.js',
-], function ($, Backbone, _, L, Cluster, MAP, Parameters, Shared, CountrySearch, CountryStatistic, Search, App) {
-    shared.dispatcher = _.extend({}, Backbone.Events);
-    parameters = new Parameters();
-    map = new MAP();
+require(['./common'], function (common) {
+   require([
+        'jquery',
+        'backbone',
+        'underscore',
+        'leaflet',
+        'static/scripts/views/map/cluster.js',
+        'map-functionality',
+        'static/scripts/parameters.js',
+        'static/scripts/shared.js',
+        'static/scripts/views/statistic/search.js',
+        'static/scripts/views/statistic/view.js',
+        'static/scripts/views/navbar/search.js',
+        'static/scripts/views/map/app.js',
+    ], function ($, Backbone, _, L, Cluster, MAP, Parameters, Shared, CountrySearch, CountryStatistic, Search, App) {
+        shared.dispatcher = _.extend({}, Backbone.Events);
+        parameters = new Parameters();
+        map = new MAP();
 
-    new App();
-    L.clusterLayer = new Cluster();
+        new App();
+        L.clusterLayer = new Cluster();
 
-    $(document).ready(function () {
-        renderCredit();
-    });
-
-        // render country statistic view
-        var countryStatictic = new CountryStatistic();
-        countryStatictic.getCount("", function (data) {
-            $('#healthsites-count').html(data);
-            $('#healthsites-count').css('opacity', 1);
+        $(document).ready(function () {
+            renderCredit();
         });
-        countryStatictic.showStatistic("");
 
-        new CountrySearch();
-        new Search();
+            // render country statistic view
+            var countryStatictic = new CountryStatistic();
+            countryStatictic.getCount("", function (data) {
+                $('#healthsites-count').html(data);
+                $('#healthsites-count').css('opacity', 1);
+            });
+            countryStatictic.showStatistic("");
+
+            new CountrySearch();
+            new Search();
     });
 });
