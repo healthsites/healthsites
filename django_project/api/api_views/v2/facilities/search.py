691e5b45716592a09a3ba019bf74e8bdf16b4200
Anita Hapsari <anita@kartoza.com>
2019-07-19 09:30:55 +0700

    add search in area (#1262)
    * add search in area
    
    * ignore exception for flake8

--- django_project/api/api_views/v2/facilities/search.py
+++ django_project/api/api_views/v2/facilities/search.py
@@ -5,14 +5,24 @@ __date__ = '15/04/19'
 from django.http.response import HttpResponseBadRequest
 from rest_framework.views import APIView, Response
 from localities_osm.serializer.locality_osm import LocalityOSMAutoCompleteSerializer
-from localities_osm.queries import all_locality
+from localities_osm.queries import all_locality, filter_locality
 
 
 class Autocomplete(APIView):
     def get(self, request):
         q = request.GET.get('q', '').capitalize()
         if len(q) > 2:
-            query = all_locality()
+            if ' in ' in q:
+                search_query = q.split(' in ')
+                country_name = search_query[1]
+                q = search_query[0]
+                try:
+                    query = filter_locality(country=country_name)
+                except:  # noqa
+                    query = all_locality()
+            else:
+                query = all_locality()
+
             search_qs = query.filter(name__icontains=q)
             serializer = \
                 LocalityOSMAutoCompleteSerializer(search_qs, many=True)
