cb69c0abde26be11b6c6e475fb19f303fb296f84
meomancer <meomancer@gmail.com>
2019-08-23 15:29:53 +0700

    add update profile and auto generate api key

--- django_project/social_users/urls.py
+++ django_project/social_users/urls.py
@@ -3,12 +3,14 @@ from django.conf.urls import include, patterns, url
 
 from .views import LogoutUser, ProfilePage, UserProfilePage, UserSigninPage
 from localities_osm_extension.views import execute_migration
+from social_users.views import ProfileUpdate
 
 urlpatterns = patterns(
     '',
     url('', include('social_django.urls', namespace='social')),
     url(r'^signin/$', UserSigninPage.as_view(), name='usersignpage'),
     url(r'^profile/$', UserProfilePage.as_view(), name='userprofilepage'),
+    url(r'^profile-update/$', ProfileUpdate.as_view(), name='userprofileupdate'),
     url(
         r'^profile/(?P<username>.*)/$', ProfilePage.as_view(),
         name='profile'
